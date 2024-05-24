52761947e60f9c23230d4fa536f585c5136068ad
NekoDesu-cyte <134627097+NekoDesu-cyte@users.noreply.github.com>
2024-05-24 12:30:53 +0700

    Update percobaan.py
    awalan dulu ye kan

--- percobaan.py
+++ percobaan.py
@@ -1,1 +1,7 @@
+kecamatan = ["Ajibarang", "Banyumas", "Baturaden", "Cilongok", "Purwokerto Barat", "Purwokerto Selatan", "Purwokerto Timur", "Sokaraja"]
 
+plastik = [160, 225, 135, 190, 255, 320, 245, 180]
+logam =[237, 183, 329, 158, 274, 195, 321, 276]
+kaca = [146, 83, 101, 58, 219, 155, 213, 180]
+elektronik = [14, 56, 120, 90, 75, 65, 78, 25]
+karet = [114, 210, 78, 98, 153, 79, 82, 126]
