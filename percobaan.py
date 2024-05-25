9dbaa5d3d45f01870ba3413644925ea54721ae82
kaka166 <hafizhfaruq858@gmail.com>
2024-05-25 14:05:27 +0700

    home

--- percobaan.py
+++ percobaan.py
@@ -9,3 +9,14 @@ karet = [114, 210, 78, 98, 153, 79, 82, 126]
 sisa_makanan = [78, 58, 12, 36, 69, 45, 87, 20]
 kotoran_hewan = [38, 92, 14, 69, 80, 24, 56, 73]
 dedaunan = [53, 21, 84, 10, 76, 33, 95, 47]
+
+def home():
+    #while True:
+        print("BANK SAMPAH YANG ADA DI KABUPATEN BANYUMAS")
+        print ("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+        for i in range(len(kecamatan)) :
+            print(f"{i+1} {kecamatan[i]}")
+        kembali()
+
+def kembali():
+      print("")
