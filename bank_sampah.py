3d97fbd7562c1ce51f634af77cbf38311cb371fc
NekoDesu-cyte <134627097+NekoDesu-cyte@users.noreply.github.com>
2024-05-31 20:28:26 +0700

    Update bank_sampah.py

--- bank_sampah.py
+++ bank_sampah.py
@@ -35,6 +35,40 @@ def kembali():
         print(f"ORGANIK = {plastik[1]+logam[1]+kaca[1]+elektronik[1]+karet[1]} Kg" )
         print("NON-ORGANIK = 400 kg" )
         print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+     elif Pemilihan == 3:
+        print("KECAMATAN BATURADEN : ")
+        print(f"ORGANIK = {plastik[2]+logam[3]+kaca[3]+elektronik[3]+karet[3]} Kg" )
+        print("NON-ORGANIK = 400 kg" )
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+    elif Pemilihan == 4:
+        print("KECAMATAN CILONGOK : ")
+        print(f"ORGANIK = {plastik[4]+logam[4]+kaca[4]+elektronik[4]+karet[4]} Kg" )
+        print("NON-ORGANIK = 400 kg" )
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+    elif Pemilihan == 5:
+        print("KECAMATAN PURWOKERTO BARAT : ")
+        print(f"ORGANIK = {plastik[5]+logam[5]+kaca[5]+elektronik[5]+karet[5]} Kg" )
+        print("NON-ORGANIK = 400 kg" )
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+    elif Pemilihan == 6:
+        print("KECAMATAN PURWOKERTO SELATAN : ")
+        print(f"ORGANIK = {plastik[6]+logam[6]+kaca[6]+elektronik[6]+karet[6]} Kg" )
+        print("NON-ORGANIK = 400 kg" )
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+    elif Pemilihan == 7:
+        print("KECAMATAN PURWOKERTO TIMUR : ")
+        print(f"ORGANIK = {plastik[7]+logam[7]+kaca[7]+elektronik[7]+karet[7]} Kg" )
+        print("NON-ORGANIK = 400 kg" )
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+    elif Pemilihan == 8:
+        print("KECAMATAN SOKARAJA : ")
+        print(f"ORGANIK = {plastik[8]+logam[8]+kaca[8]+elektronik[8]+karet[8]} Kg" )
+        print("NON-ORGANIK = 400 kg" )
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+    else:
+        print("TOLONG MASUKAN ANGKA SESUAI DENGAN YANG TERSEDIA!") 
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+        home()
 
 def tampilkanMenu():
         for t in range(1):
