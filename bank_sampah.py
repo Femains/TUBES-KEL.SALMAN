8840cec90eef44448f2e0c78353d29d292bec2de
DuckyDash <111984726+DuckyDash@users.noreply.github.com>
2024-06-01 15:53:26 +0700

    Update bank_sampah.py
    Removing unecessary long code

--- bank_sampah.py
+++ bank_sampah.py
@@ -33,52 +33,17 @@ def kembali():
     print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
     Pemilihan = int(input("PILIH KECAMATAN YANG ADA DI KABUPATEN BANYUMAS => "))
     print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-    if Pemilihan == 1:
-        print("--KECAMATAN AJIBARANG--")
-        print(f"ORGANIK =  {sisa_makanan[0]+kotoran_hewan[0]+dedaunan[0]}Kg" )
-        print(f"NON-ORGANIK = {plastik[0]+logam[0]+kaca[0]+elektronik[0]+karet[0]}Kg " )
+    if Pemilihan in range(1, len(kecamatan) + 1):
+        i = Pemilihan - 1
+        print(f"--KECAMATAN {kecamatan[i].upper()}--")
+        print(f"ORGANIK = {sisa_makanan[i] + kotoran_hewan[i] + dedaunan[i]} Kg")
+        print(f"NON-ORGANIK = {plastik[i] + logam[i] + kaca[i] + elektronik[i] + karet[i]} Kg")
         print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
         tampilkanMenu()
-    elif Pemilihan == 2:
-        print("KECAMATAN BANYUMAS : ")
-        print(f"ORGANIK = {plastik[1]+logam[1]+kaca[1]+elektronik[1]+karet[1]} Kg" )
-        print("NON-ORGANIK = 400 kg" )
-        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-    elif Pemilihan == 3:
-        print("KECAMATAN BATURADEN : ")
-        print(f"ORGANIK = {plastik[2]+logam[3]+kaca[3]+elektronik[3]+karet[3]} Kg" )
-        print("NON-ORGANIK = 400 kg" )
-        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-    elif Pemilihan == 4:
-        print("KECAMATAN CILONGOK : ")
-        print(f"ORGANIK = {plastik[4]+logam[4]+kaca[4]+elektronik[4]+karet[4]} Kg" )
-        print("NON-ORGANIK = 400 kg" )
-        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-    elif Pemilihan == 5:
-        print("KECAMATAN PURWOKERTO BARAT : ")
-        print(f"ORGANIK = {plastik[5]+logam[5]+kaca[5]+elektronik[5]+karet[5]} Kg" )
-        print("NON-ORGANIK = 400 kg" )
-        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-    elif Pemilihan == 6:
-        print("KECAMATAN PURWOKERTO SELATAN : ")
-        print(f"ORGANIK = {plastik[6]+logam[6]+kaca[6]+elektronik[6]+karet[6]} Kg" )
-        print("NON-ORGANIK = 400 kg" )
-        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-    elif Pemilihan == 7:
-        print("KECAMATAN PURWOKERTO TIMUR : ")
-        print(f"ORGANIK = {plastik[7]+logam[7]+kaca[7]+elektronik[7]+karet[7]} Kg" )
-        print("NON-ORGANIK = 400 kg" )
-        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-    elif Pemilihan == 8:
-        print("KECAMATAN SOKARAJA : ")
-        print(f"ORGANIK = {plastik[8]+logam[8]+kaca[8]+elektronik[8]+karet[8]} Kg" )
-        print("NON-ORGANIK = 400 kg" )
-        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
     else:
-        print("TOLONG MASUKAN ANGKA SESUAI DENGAN YANG TERSEDIA!") 
+        print("TOLONG MASUKAN ANGKA SESUAI DENGAN YANG TERSEDIA!")
         print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
         home()
-
 def tampilkanMenu():
         for t in range(1):
             print(f"{t+1}. Jenis Sampah")
