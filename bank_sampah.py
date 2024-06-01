3c4823da5813e9993474e59a96ea318a6fa284a6
Femains <salmanhascil116@gmail.com>
2024-06-01 16:17:12 +0700

    Update Bank Sampah

--- bank_sampah.py
+++ bank_sampah.py
@@ -1,3 +1,4 @@
+import sys
 
 kecamatan = ["Ajibarang", "Banyumas", "Baturaden", "Cilongok", "Purwokerto Barat", "Purwokerto Selatan", "Purwokerto Timur", "Sokaraja"]
 
@@ -10,38 +11,180 @@ karet = [114, 210, 78, 98, 153, 79, 82, 126]
 sisa_makanan = [78, 58, 12, 36, 69, 45, 87, 20]
 kotoran_hewan = [38, 92, 14, 69, 80, 24, 56, 73]
 dedaunan = [53, 21, 84, 10, 76, 33, 95, 47]
+gabung = []
 
 def home():
-    #while True:
-        print("BANK SAMPAH YANG ADA DI KABUPATEN BANYUMAS")
-        print ("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-        for i in range(len(kecamatan)) :
-            print(f"{i+1} {kecamatan[i]}")
-        kembali()
+    print("BANK SAMPAH YANG ADA DI KABUPATEN BANYUMAS")
+    print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+    print("1. List Bank Sampah")
+    print("2. Sortir Sampah")
+    menu_pilih = int(input("Masukan menu yang diinginkan : "))
+    if menu_pilih == 1:
+        list_kecamatan()
+    elif menu_pilih == 2:
+        tampilkanSortir()
+
+def list_kecamatan():
+    for i in range(len(kecamatan)):
+        print(f"{i+1} {kecamatan[i]}")
+    kembali()
 
 def kembali():
     print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
     Pemilihan = int(input("PILIH KECAMATAN YANG ADA DI KABUPATEN BANYUMAS => "))
     print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-    if Pemilihan == 1:
-        print("--KECAMATAN AJIBARANG--")
-        print(f"ORGANIK =  {sisa_makanan[0]+kotoran_hewan[0]+dedaunan[0]}Kg" )
-        print(f"NON-ORGANIK = {plastik[0]+logam[0]+kaca[0]+elektronik[0]+karet[0]}Kg " )
+    if Pemilihan in range(1, len(kecamatan) + 1):
+        i = Pemilihan - 1
+        print(f"--KECAMATAN {kecamatan[i].upper()}--")
+        print(f"ORGANIK = {sisa_makanan[i] + kotoran_hewan[i] + dedaunan[i]} Kg")
+        print(f"NON-ORGANIK = {plastik[i] + logam[i] + kaca[i] + elektronik[i] + karet[i]} Kg")
         print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
         tampilkanMenu()
-    elif Pemilihan == 2:
-        print("KECAMATAN BANYUMAS : ")
-        print(f"ORGANIK = {plastik[1]+logam[1]+kaca[1]+elektronik[1]+karet[1]} Kg" )
-        print("NON-ORGANIK = 400 kg" )
+    else:
+        print("TOLONG MASUKAN ANGKA SESUAI DENGAN YANG TERSEDIA!")
         print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
-
+        home()
 def tampilkanMenu():
-    while True:
         for t in range(1):
-            print(f"{t+1} Jenis Sampah")
-            print(f"{t+2} KEMBALI")
-            print(f"{t+3} KELUAR")
+            print(f"{t+1}. Jenis Sampah")
+            print(f"{t+2}. KEMBALI")
+            print(f"{t+3}. KELUAR")
             print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
             
-home()
+        pilihan = int(input("MASUKKAN PILIHAN => "))
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+        if pilihan == 1:
+            tampilkanSampah()
+        elif pilihan == 2:
+            home()
+        elif pilihan == 3:
+            sys.exit("Keluar dari program, terima kasih telah menggunakan program kami")
+        else:
+            print("Input yang dimasukan invalid, harap coba kembali")
 
+def tampilkanSampah():
+    print("---ORGANIK---")
+    print(f"SISA MAKANAN = {sisa_makanan[0]} Kg")
+    print(f"KOTORAN HEWAN = {kotoran_hewan[0]} Kg")
+    print(f"DEDAUNAN = {dedaunan[0]} Kg")
+    print("─━──━──━──━──━──━──━──━──━──━─")
+    print("--NON-ORGANIK--")
+    print(f"PLASTIK = {plastik[0]} Kg")
+    print(f"LOGAM = {logam[0]} Kg")
+    print(f"KACA = {kaca[0]} Kg")
+    print(f"ELEKTRONIK = {elektronik[0]} Kg")
+    print(f"KARET = {karet[0]} Kg")
+    print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+
+    while True:
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+        print("1. KEMBALI KE LIST KECAMATAN")
+        print("2. HOME")
+        print("3. KELUAR")
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+
+        pilihan1 = int(input("MASUKKAN PILIHAN => "))
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+        if pilihan1 == 1:
+            list_kecamatan()
+        elif pilihan1 == 2:
+            home()
+        elif pilihan1 == 3:
+            sys.exit("Keluar dari program, terima kasih telah menggunakan program kami")
+        else:
+            print("Input yang dimasukan invalid, harap coba kembali")
+
+def semua(jenis):
+    for i in range(len(kecamatan)):
+        gabung.append((kecamatan[i], jenis[i]))
+    
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        item = array[i]
+        j = i - 1
+        while j >= 0 and array[j][1] < item[1]:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = item
+    return array
+
+def tampilkanSortir():
+    jenis_sampah = ["Plastik", "Logam", "Kaca", "Elektronik", "Karet", "Sisa Makanan", "Kotoran Hewan", "Dedaunan"]
+
+
+    print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+    print("Pilih jenis sampah untuk disortir:")
+    print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+    for i in range(len(jenis_sampah)) :
+        print(f"{i+1} {jenis_sampah[i]} ")
+
+    pilihan = int(input("MASUKKAN PILIHAN => "))
+    if pilihan in range(1, 9): 
+        gabung.clear()
+
+    if pilihan == 1 :
+        semua(plastik)
+        print("")
+        for kecamatan, berat in insertion_sort(gabung):
+            print(f"- {kecamatan} {berat} kg")
+        
+    elif pilihan == 2 :
+        semua(logam)
+        print("")
+        for kecamatan, berat in insertion_sort(gabung):
+            print(f"- {kecamatan} {berat} kg")
+    elif pilihan == 3 :
+        semua(kaca)
+        print("")
+        for kecamatan, berat in insertion_sort(gabung):
+            print(f"- {kecamatan} {berat} kg")
+    elif pilihan == 4 :
+        semua(elektronik)
+        print("")
+        for kecamatan, berat in insertion_sort(gabung):
+            print(f"- {kecamatan} {berat} kg")
+    elif pilihan == 5 :
+        semua(karet)
+        print("")
+        for kecamatan, berat in insertion_sort(gabung):
+            print(f"- {kecamatan} {berat} kg")
+    elif pilihan == 6 :
+        semua(sisa_makanan)
+        print("")
+        for kecamatan, berat in insertion_sort(gabung):
+            print(f"- {kecamatan} {berat} kg")
+    elif pilihan == 7 :
+        semua(kotoran_hewan)
+        print("")
+        for kecamatan, berat in insertion_sort(gabung):
+            print(f"- {kecamatan} {berat} kg")
+    elif pilihan == 8 :
+        semua(dedaunan)
+        print("")
+        for kecamatan, berat in insertion_sort(gabung):
+            print(f"- {kecamatan} {berat} kg")
+    
+
+    else:
+        print("Input yang dimasukan invalid, harap coba kembali")
+
+    while True:
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+        print("1. KEMBALI")
+        print("2. KELUAR")
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+
+        pilihan1 = int(input("MASUKKAN PILIHAN => "))
+        print("─━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━──━─")
+        if pilihan1 == 1:
+            print(" ")
+            print(" ")
+            print(" ")
+            tampilkanSortir()
+
+        elif pilihan1 == 2:
+            sys.exit("Keluar dari program, terima kasih telah menggunakan program kami")
+        else:
+            print("Input yang dimasukan invalid, harap coba kembali")
+    
+home()
